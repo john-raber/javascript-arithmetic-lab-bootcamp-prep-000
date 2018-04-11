71f04e979b00df0e12e09a613851cbb8ef9a9633
John Raber <jrab224@gmail.com>
2018-04-11 01:39:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,15 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return 
+  return n += 1
 }
+
+function dec(n) {
+  return n -= 1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function 
